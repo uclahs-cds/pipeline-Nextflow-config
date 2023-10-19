7bb7dda17de57407c4d4d104d15ef676a766ec6e
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2023-10-19 15:05:10 -0700

    Make the test runner actually complain

--- tests/suite.groovy
+++ tests/suite.groovy
@@ -1,3 +1,16 @@
+// https://stackoverflow.com/a/4115972
 import org.junit.runner.JUnitCore
 
-JUnitCore.runClasses SetEnvTests
+result = JUnitCore.runClasses SetEnvTests
+
+String message = "Ran: " + result.getRunCount() + ", Ignored: " + result.getIgnoreCount() + ", Failed: " + result.getFailureCount()
+if (result.wasSuccessful()) {
+    println "SUCCESS! " + message
+} else {
+    println "FAILURE! " + message
+    result.getFailures().each {
+        println "Test Failure: ${it.getTestHeader()}"
+        println it.getException()
+    }
+    System.exit(1)
+}
